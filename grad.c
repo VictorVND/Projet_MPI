24a3d9287bc976f413005f0b8485fc1d067a339b
ayalf <66946732+ayalf@users.noreply.github.com>
2022-04-05 17:55:51 +0200

    Create grad.c

---
+++ grad.c
@@ -0,0 +1,15 @@
+
+void matvec(double*A,double*U, int Nx, int Ny)
+{
+
+  for (int i = 0; i < Nx; i++)
+  {
+      for (int j = 0; j < Ny; i++)
+      {
+          /* code */
+      }
+      
+  }
+
+
+}
