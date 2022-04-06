9acd17e08b9ba502589e550da2cbd6d50e9dcdc7
ayalf <ayat.aya.aa@.com>
2022-04-06 11:37:36 +0200

    Produit matrice vecteur #

--- grad.c
+++ grad.c
@@ -1,12 +1,55 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include"grad.h"
 
-void matvec(double*A,double*U, int Nx, int Ny)
+
+
+void matvec(double*A,double*U,double*P, int Nx, int Ny)
 {
 
   for (int i = 0; i < Nx; i++)
   {
       for (int j = 0; j < Ny; i++)
       {
-          /* code */
+          if (i=1 && (j¡=1 || j¡=Ny))
+        {
+         P[(j-1)*Nx+(i-1)]=a*U[(j-1)*Nx+(i-1)]+b*(U[(j-1)*Nx+(i)])+c*(U[(j-2)*Nx+(i-1)]+U[(j)*Nx+(i-1)]);
+        }
+          if (i=Nx && (j¡=1 || j¡=Ny))
+        {
+         P[(j-1)*Nx+(i-1)]=a*U[(j-1)*Nx+(i-1)]+b*(U[(j-1)*Nx+(i-2)])+c*(U[(j-2)*Nx+(i-1)]+U[(j)*Nx+(i-1)]);
+        }
+          if (j=Ny && (i¡=1 || i¡=Nx))
+        {
+         P[(j-1)*Nx+(i-1)]=a*U[(j-1)*Nx+(i-1)]+b*(U[(j-1)*Nx+(i-2)]+U[(j-1)*Nx+(i)])+c*(U[(j-2)*Nx+(i-1)]);
+        }
+          if (j=1 && (i¡=1 || i¡=Nx))
+        {
+         P[(j-1)*Nx+(i-1)]=a*U[(j-1)*Nx+(i-1)]+b*(U[(j-1)*Nx+(i-2)]+U[(j-1)*Nx+(i)])+c*(U[(j)*Nx+(i-1)]);
+        }
+          if (i=1 && j=1)
+        {
+         P[(j-1)*Nx+(i-1)]=a*U[(j-1)*Nx+(i-1)]+b*(U[(j-1)*Nx+(i)])+c*(U[(j)*Nx+(i-1)]);
+        }
+           if (i=1 && j=Ny)
+        {
+         P[(j-1)*Nx+(i-1)]=a*U[(j-1)*Nx+(i-1)]+b*(U[(j-1)*Nx+(i)])+c*(U[(j-2)*Nx+(i-1)]);
+        }
+           if (i=Nx && j=Ny)
+        {
+         P[(j-1)*Nx+(i-1)]=a*U[(j-1)*Nx+(i-1)]+b*(U[(j-1)*Nx+(i-2)])+c*(U[(j-2)*Nx+(i-1)]);
+        }
+           if (i=Nx && j=1)
+        {
+         P[(j-1)*Nx+(i-1)]=a*U[(j-1)*Nx+(i-1)]+b*(U[(j-1)*Nx+(i-2)])+c*(U[(j)*Nx+(i-1)]);
+        }
+        else
+        {
+          P[(j-1)*Nx+(i-1)]=a*U[(j-1)*Nx+(i-1)]+b*(U[(j-1)*Nx+(i-2)]+U[(j-1)*Nx+(i)])+c*(U[(j-2)*Nx+(i-1)]+U[(j)*Nx+(i-1)]);
+        }
+        
+        
+        
       }
       
   }
