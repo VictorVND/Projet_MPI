d00ace148245e8b528bf54726fb21588d38632d7
ayalf <ayat.aya.aa@.com>
2022-04-11 19:53:16 +0200

    les 3 cas de fonctions

--- fonctions.h
+++ fonctions.h
@@ -1,3 +1,15 @@
-double f(double x,double y,double t);
-double g(double x,double y,double t);
-double h(double x,double y,double t);
+#include<stdlib.h>
+#include<stdio.h>
+#define PI 3.14
+#define Lx 1.0
+#define Ly 1.0
+
+double f1(double x,double y,double t);
+double g1(double x,double y,double t);
+double h1(double x,double y,double t);
+double f2(double x,double y,double t);
+double g2(double x,double y,double t);
+double h2(double x,double y,double t);
+double f3(double x,double y,double t);
+double g3(double x,double y,double t);
+double h3(double x,double y,double t);
