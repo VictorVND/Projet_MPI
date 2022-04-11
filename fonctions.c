df6e6710db7cef3b59f16852588196bfc55a834a
ayalf <ayat.aya.aa@.com>
2022-04-11 19:50:48 +0200

    les 3 cas de fonctions

--- fonctions.c
+++ fonctions.c
@@ -1,15 +1,39 @@
+#include "fonctions.h"
 
-
-double f(double x,double y,double t){
+double f1(double x,double y,double t){
     return 2*(y - y*y + x - x*x);
 }
 
-double g(double x,double y,double t){
+double g1(double x,double y,double t){
     return 0.0;
 }
 
-double h(double x,double y,double t){
+double h1(double x,double y,double t){
     return 0.0;
+}
+
+double f2(double x,double y,double t){
+    return (sin(x)+ cos(y));
+}
+
+double g2(double x,double y,double t){
+    return (sin(x)+ cos(y));
+}
+
+double h2(double x,double y,double t){
+    return (sin(x)+ cos(y));
+}
+
+double f3(double x,double y,double t){
+    return exp(-(x-Lx/2)*(x-Lx/2))*exp(-(y-Ly/2)*(y-Ly/2))*cos(PI*t/2);
+}
+
+double g3(double x,double y,double t){
+    return 0.0;
+}
+
+double h3(double x,double y,double t){
+    return 1.0;
 }
 
 
