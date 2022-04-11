43a131e202805929b88c1f704617e5b163878613
VictorVND <94636658+VictorVND@users.noreply.github.com>
2022-04-11 23:35:09 +0200

    Add files via upload

--- fonctions.c
+++ fonctions.c
@@ -1,39 +1,33 @@
-#include "fonctions.h"
 
-double f1(double x,double y,double t){
+
+double fct_f(double x,double y,double t){
     return 2*(y - y*y + x - x*x);
 }
 
-double g1(double x,double y,double t){
+double fct_g(double x,double y,double t){
     return 0.0;
 }
 
-double h1(double x,double y,double t){
+double fct_h(double x,double y,double t){
     return 0.0;
 }
 
-double f2(double x,double y,double t){
-    return (sin(x)+ cos(y));
-}
+void read_data(int*Nx, int*Ny,double*Lx,double*Ly,double*D,double*dt){
+    
+    FILE *fptr;
 
-double g2(double x,double y,double t){
-    return (sin(x)+ cos(y));
-}
+    fptr = fopen("donnees.dat", "r");
 
-double h2(double x,double y,double t){
-    return (sin(x)+ cos(y));
-}
+    Nx = getw(fptr);
+    Ny = getw(fptr);
+    Lx = getw(fptr);
+    Ly = getw(fptr);
+    D = getw(fptr);
+    dt = getw(fptr);
 
-double f3(double x,double y,double t){
-    return exp(-(x-Lx/2)*(x-Lx/2))*exp(-(y-Ly/2)*(y-Ly/2))*cos(PI*t/2);
-}
+    fclose(fptr);
 
-double g3(double x,double y,double t){
-    return 0.0;
-}
-
-double h3(double x,double y,double t){
-    return 1.0;
+    return;
 }
 
 
